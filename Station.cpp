902c3fec706cc417c0622c90b60c19a25660d3ce
Mark_Ignatchik <Ignatchik_MA_25@student.grsu.by>
2026-05-26 18:48:13 +0300

    Хранение и использование станций используя json

--- Station.cpp
+++ Station.cpp
@@ -2,6 +2,6 @@
 #include <cmath>
 using namespace std;
 double Distance(const Station& From, const Station& To) {
-    double d = sqrt(pow((To.x - From.x), 2) + pow((To.y - From.y), 2));
-    return d;
+    double stationDistance = sqrt(pow((To.x - From.x), 2) + pow((To.y - From.y), 2));
+    return stationDistance;
 }
