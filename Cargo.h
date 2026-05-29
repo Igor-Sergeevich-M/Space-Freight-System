5a7374d6d08ec48f789461def87f19bf39c1cb93
Игорь Мартинович <toptopcik247@gmail.com>
2026-05-29 09:43:33 +0300

    Изменена структура Cargo.h
    В связи с изменением логики подбора груза изменена структура Cargo.

--- Cargo.h
+++ Cargo.h
@@ -1,12 +1,12 @@
 #pragma once
 #include <string>
-// Объявляем структуру для хранения данных о грузе
+
 struct Cargo {
     std::string cargoName;
-    int cargoMass;
-    int cargoGabarits;   // Объём в м^3
-    bool cargoPasanger;
-    bool cargoCreature;   // Животные/растения
-    bool cargoAmaxophobe; // Чувствительность к скорости > 25 СС
-    bool cargoDanger;     // Опасный груз
+    int cargoMass = 0;
+    int cargoGabarits = 0;
+    bool cargoPasanger = false;
+    bool cargoCreature = false;
+    bool cargoAmaxophobe = false;
+    bool cargoDanger = false;
 };
